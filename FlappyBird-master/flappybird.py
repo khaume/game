53b01620718fa3f81e5e5ff4face3b28b571d5aa
Kaspar Haume (KHH) <KHH@kmd.dk>
2019-01-22 16:24:01 +0100

    Working on flappy

--- FlappyBird-master/flappybird.py
+++ FlappyBird-master/flappybird.py
@@ -20,7 +20,7 @@ class FlappyBird:
         self.gamma = 0.5
         self.learning_rate = 0.1
 
-        self.screen = pygame.display.set_mode((400, 708))
+        self.screen = pygame.display.set_mode((400, 700))
         self.bird = pygame.Rect(65, 50, 50, 50)
         self.background = pygame.image.load("assets/background.png").convert()
         self.birdSprites = [pygame.image.load("assets/1.png").convert_alpha(),
@@ -38,28 +38,37 @@ class FlappyBird:
         self.dead = False
         self.sprite = 0
         self.counter = 0
-        self.offset = random.randint(-110, 110)
+        # self.offset = random.randint(-110, 110)
+        self.offset = 0
 
         self.centerdot = pygame.Surface((5, 5))
         self.centerdot.fill((0, 0, 0))
 
         # Dim: action, dy, dx
-        self.R = np.zeros((2, 120, 48))
+        self.R = np.zeros((2, 70, 48))
 
         # Add reward when bird is in middle of gap and jumps while passing walls
         self.R[1, self.dy_to_index(0), self.dx_to_index(0)] = 100
 
+        # EDIT: Below is removed because it is too much manipulation.
+        # Should happen while updating Q matrix
         # Add punishment when hitting wall inside gap
-        for x in range(0, 80):
-            xindex = self.dx_to_index(x)
+        # for x in range(0, 80):
+        #     xindex = self.dx_to_index(x)
+        #
+        #     self.R[:, self.dy_to_index(-64), xindex] = -100
+        #
+        #     self.R[:, self.dy_to_index(65), xindex] = -100
+        for y in range(350, 435):
+            yindex = self.dy_to_index(y)
 
-            self.R[:, self.dy_to_index(-64), xindex] = -100
+            self.R[1, yindex, :] = -100
 
-            self.R[:, self.dy_to_index(65), xindex] = -100
+        self.Q = np.zeros((2, 120, 48))
+        # self.Q = np.random.rand(2, 120, 48)
+        # self.Q[1, :, :] *= 0.1
 
-        # self.Q = np.zeros((2, 120, 48))
-        self.Q = np.random.rand(2, 120, 48)
-        self.Q[1, :, :] *= 0.1
+        print(self.R[1, 60:70, :5])
 
     def train_run(self):
 
@@ -67,15 +76,18 @@ class FlappyBird:
         clock = pygame.time.Clock()
         font = pygame.font.SysFont("Arial", 50)
         while self.gen_counter < self.max_gens:
-            self.step_counter += 1
+            print('='*20)
+            if not self.dead:
 
-            clock.tick(200)
+                self.step_counter += 1
 
-            x_index = self.dx_to_index(self.bird_wall_dist())
-            y_index = self.dy_to_index(self.delta_y())
+                clock.tick(2)
 
-            # print('delta y :', self.delta_y(), 'yindex: ', y_index)
-            if self.delta_y() < self.gap / 2 and not self.dead:
+                x_index = self.dx_to_index(self.bird_wall_dist())
+                y_index = self.dy_to_index(self.delta_y())
+
+                # print('delta y :', self.delta_y(), 'yindex: ', y_index)
+                # if self.delta_y() < self.gap / 2 and not self.dead:
 
                 # Random chance to pick random move, otherwise pick max Q
                 action = None
@@ -93,7 +105,7 @@ class FlappyBird:
                     max_Q = np.max(self.Q[:, y_index, x_index])
                     action_list = np.argwhere(self.Q[:, y_index, x_index] == max_Q).flatten().tolist()
 
-                    # action_list = random.sample(action_list, len(action_list))
+                    action_list = random.sample(action_list, len(action_list))
                     print('non random actions are ', action_list)
 
                 print('going to state change')
@@ -101,7 +113,8 @@ class FlappyBird:
                 for potential_action in action_list:
                     action = potential_action
 
-                    if self.R[action, y_index, x_index] == -1:
+                    if self.R[action, y_index, x_index] < 0:
+                        self.Q[action, y_index, x_index] = -1
                         continue
 
                     else:
@@ -122,6 +135,8 @@ class FlappyBird:
 
                 print('updating {}, {}, {} to '.format(action, new_y_index, x_index), ' to ',
                       self.Q[action, new_y_index, x_index])
+
+                print('dy: ', self.delta_y())
 
                 if action:
                     self.jump = 17
@@ -165,11 +180,11 @@ class FlappyBird:
         return gap_center - self.birdY
 
     def dx_to_index(self, dx):
-        xmin = -193.9
+        xmin = -193.9  # When the wall is drawn at -80, the bird is at 70, bird is 44 wide
         return int(dx - xmin) // 10
 
     def dy_to_index(self, dy):
-        ymin = -599.9
+        ymin = -264.9
         return int(dy - ymin) // 10
 
     def updateWalls(self):
@@ -190,7 +205,7 @@ class FlappyBird:
             self.gravity += 0.2
         self.bird[1] = self.birdY
         upRect = pygame.Rect(self.wallx,
-                             360 + self.gap - self.offset + 10,
+                             370 + self.gap - self.offset + 10,
                              self.wallUp.get_width() - 10,
                              self.wallUp.get_height())
         downRect = pygame.Rect(self.wallx,
@@ -209,10 +224,11 @@ class FlappyBird:
             self.dead = False
             self.counter = 0
             self.wallx = 400
-            self.offset = random.randint(-110, 110)
+            # self.offset = random.randint(-110, 110)
+            self.offset = 0
             self.gravity = 5
 
-            print(self.Q)
+            # print(self.Q)
 
     def state_monitor(self):
         gap_center = 0 - self.gap / 2 + 500 - self.offset
@@ -228,16 +244,16 @@ class FlappyBird:
                 if event.type == pygame.QUIT:
                     sys.exit()
                 if (event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN) and not self.dead:
-                    self.jump = 17
+                    self.jump = 15
                     self.gravity = 5
                     self.jumpSpeed = 10
 
             self.screen.fill((255, 255, 255))
             self.screen.blit(self.background, (0, 0))
-            self.screen.blit(self.wallUp,
-                             (self.wallx, 360 + self.gap - self.offset))
             self.screen.blit(self.wallDown,
                              (self.wallx, 0 - self.gap - self.offset))
+            self.screen.blit(self.wallUp,
+                             (self.wallx, 370 + self.gap - self.offset))
             self.screen.blit(font.render(str(self.counter),
                                          -1,
                                          (255, 255, 255)),
